caf78e386e89ca047d5684820a7b860ef5f3f5e8
Abdi Rahman <abdirinoahash@gmail.com>
2024-11-03 11:07:32 +0300

    clerk problem

--- src/app/(marketing)/_components/Navbar.tsx
+++ src/app/(marketing)/_components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Logo } from "@/components/Logo";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 export function Navbar() {
@@ -12,7 +13,12 @@ export function Navbar() {
         <Link className="text-lg" href="/#pricing">Pricing</Link>
         <Link className="text-lg" href="/#">About</Link>
         <span className="text-lg">
-            
+            <SignedIn>
+              <Link href="/dashboard">Dashboard</Link>
+            </SignedIn>
+            <SignedOut>
+              <SignInButton>Login</SignInButton>
+            </SignedOut>
         </span>
       </nav>
     </header>
